38661c552bbb1838e0c2fb449029683dab3e484d
syedtarif <s.tarif2000@gmail.com>
2024-03-14 12:26:17 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <main className='md:flex'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </main>
+    </>
+  )
+}
+
+export default App
